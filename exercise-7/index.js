578a527928b65eeec19026cfc383f9786ef63906
ngokhang <ngokhang2002@gmail.com>
2023-03-06 19:03:43 +0700

    finish ex 7

---
+++ exercise-7/index.js
@@ -0,0 +1,16 @@
+let array = [1, 2, 3, 3, 2, 3, 3, 5, 6, 7, 7, 7, 4, 4];
+
+function convertArrayToObject(array) {
+  if (!Array.isArray(array) || array.length === 0) return false;
+
+  return array.reduce((pre, curr) => {
+    if (pre[curr]) {
+      pre[curr]++;
+    } else {
+      pre[curr] = 1;
+    }
+    return pre;
+  }, {});
+};
+
+console.log(convertArrayToObject(array));
